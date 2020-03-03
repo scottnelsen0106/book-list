86ed304ad73ea1c3b2bb500aa34e01cc36c126e1
Scott Nelsen <snelsen0106@gmail.com>
2020-03-02 19:42:27 -0600

    console logging correct input information

---
+++ app.js
@@ -0,0 +1,57 @@
+class Book {
+    constructor(title, author, isbn) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+    }
+}
+
+class UI {
+    static displayBooks() {
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'John Doe',
+                isbn: '12121212'
+            },
+            {
+                title: 'Book Two',
+                author: 'Jane Doe',
+                isbn: '1324244'
+            }
+        ];
+
+        const books = StoredBooks;
+
+        books.forEach((book) => UI.addBookToList(book));
+    }
+
+    static addBookToList(book) {
+        const list = document.querySelector("#book-list");
+
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+        <td>${book.title}</td>
+        <td>${book.author}</td>
+        <td>${book.isbn}</td>
+        <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>
+        `;
+
+        list.appendChild(row);
+    }
+}
+
+//Display Book
+document.addEventListener("DOMContentLoaded", UI.displayBooks);
+
+document.querySelector("#book-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#title").value;
+    const author = document.querySelector("#author").value;
+    const isbn = document.querySelector("#isbn").value;
+
+    const book = new Book(title, author, isbn);
+    console.log(book);
+});
