d45a5fc06fed236f5a8be36aeef8ea20ce1fde90
Scott Nelsen <snelsen0106@gmail.com>
2020-03-02 21:15:48 -0600

    css successfully linked

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ class UI {
         const div = document.createElement('div');
         div.className = `alert alert-${className}`;
         div.appendChild(document.createTextNode(message));
-        const container = document.querySelector('.jumbotron');
+        const container = document.querySelector('.container');
         const form = document.querySelector('#book-form');
         container.insertBefore(div, form);
         setTimeout(() => document.querySelector('.alert').remove(), 3000);
